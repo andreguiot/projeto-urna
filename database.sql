b2443372aa8e064876acc862f297a26817635b2e
André De Martin Guiot <demartinguiotandre@gmail.com>
2026-02-21 11:43:22 -0300

    feat: implementado lógica por sexo e numero de chapa auto-increment

--- database.sql
+++ database.sql
@@ -1,25 +1,33 @@
--- Tabela para armazenar os candidatos
-CREATE TABLE IF NOT EXISTS candidatos (
+DROP TABLE IF EXISTS candidatos CASCADE;
+
+-- Nova coluna sexo e numero chapa auto-increment
+CREATE TABLE candidatos (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    numero_chapa VARCHAR(10) NOT NULL,
-    turma VARCHAR(10) NOT NULL,
+    sexo VARCHAR(20) NOT NULL,     
+    numero_chapa INTEGER NOT NULL,
+    turma VARCHAR(20) NOT NULL,
     caminho_foto VARCHAR(255),
     votos INTEGER DEFAULT 0,
     CONSTRAINT unq_candidato_turma UNIQUE (numero_chapa, turma)
 );
 
--- Garante a regra de negócio de uma inserção segura
+-- Procedure com Auto-Incremento por turma
 CREATE OR REPLACE FUNCTION sp_cadastrar_candidato(
     p_nome VARCHAR,
-    p_numero VARCHAR,
+    p_sexo VARCHAR,
     p_turma VARCHAR,
     p_foto_path VARCHAR
 ) RETURNS VOID AS $$
+DECLARE
+    v_proximo_numero INTEGER;
 BEGIN
-    INSERT INTO candidatos (nome, numero_chapa, turma, caminho_foto)
-    VALUES (p_nome, p_numero, p_turma, p_foto_path);
+    SELECT COALESCE(MAX(numero_chapa), 0) + 1 INTO v_proximo_numero
+    FROM candidatos
+    WHERE turma = p_turma;
+    INSERT INTO candidatos (nome, sexo, numero_chapa, turma, caminho_foto)
+    VALUES (p_nome, p_sexo, v_proximo_numero, p_turma, p_foto_path);
 END;
 $$ LANGUAGE plpgsql;
 
-select * from candidatos
+select * from candidatos;
